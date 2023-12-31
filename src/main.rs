4999b13e360016ae0afc722f517064c51b63cf7c
Stijn Gijsen <stijn.gijsen@gmail.com>
2023-12-31 15:21:50 +0100

    Remove old todo comments

--- src/main.rs
+++ src/main.rs
@@ -45,10 +45,3 @@ fn load_puzzle(file_path: &str) -> data::Grid {
     }
     grid
 }
-
-// Set up data structure: x * x cells, n regions
-// for each cell, track which regions they belong to, and what their candidates are, and their value, and whether they're given or not
-// implement calculating all candidates for the grid: for each cell, take the complement of the set of all given values in its regions
-// implement strategies for solving: singles, intersections, naked pairs...
-// implement brute force as a fallback
-// implement ways to display the state, (including step-by-step solving?)
