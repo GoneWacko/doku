c36c1e99628654598569681b6b787d53c488265f
Stijn Gijsen <stijn.gijsen@gmail.com>
2024-01-01 18:24:27 +0100

    Refactor strategy application to be a bit more DRY

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ mod data;
 mod output;
 mod strategies;
 
-use data::Coord;
+use data::{Coord, Grid};
 use strategies::naked_pair::NakedPair;
 use strategies::region_intersection::RegionIntersection;
 use strategies::single::Single;
@@ -21,56 +21,42 @@ fn main() {
     let mut grid = load_puzzle(file_path.as_str());
     grid.compute_candidates();
 
-    println!("### Initial grid:");
     let mut i: u32 = 0;
-    print_board(&grid, i);
     while !grid.is_solved() {
+        print_board(&grid, i);
         i += 1;
         println!();
         println!("Trying singles");
-        let solutions = Single::solutions(&grid);
-        if !solutions.is_empty() {
-            println!("### ({i}) Found solutions:");
-            solutions.iter().for_each(|s| println!("{s}"));
-            grid.apply_solutions(&solutions);
-            print_board(&grid, i);
+        if Single::try_solution(&mut grid) {
             continue;
         }
         println!("No singles found.");
-        println!("Trying intersection reduction");
-        let reductions = RegionIntersection::reduce_candidates(&grid);
-        if !reductions.is_empty() {
-            println!("### ({i}) Found reductions:");
-            reductions.iter().for_each(|r| println!("{r}"));
-            grid.apply_reductions(&reductions);
-            print_board(&grid, i);
+        println!("Trying intersections");
+        if RegionIntersection::try_reduction(&mut grid) {
             continue;
         }
         println!("No intersections found.");
         println!("Trying naked pair reduction");
-        let reductions = NakedPair::reduce_candidates(&grid);
-        if !reductions.is_empty() {
-            println!("### ({i}) Found naked pair reductions:");
-            reductions.iter().for_each(|r| println!("{r}"));
-            grid.apply_reductions(&reductions);
-            print_board(&grid, i);
+        if NakedPair::try_reduction(&mut grid) {
             continue;
         }
         println!("No naked pairs found.");
         print_board(&grid, i);
         panic!("No implemented strategies can further solve this board!");
     }
+    println!("Solved:");
+    print_board(&grid, i);
 }
 
-fn print_board(grid: &data::Grid, i: u32) {
+fn print_board(grid: &Grid, i: u32) {
     println!("### ({i}) Current board state:");
     output::output_grid(&grid);
     println!("### ({i}) Candidates:");
     output::output_candidates(&grid);
 }
 
-fn load_puzzle(file_path: &str) -> data::Grid {
-    let mut grid = data::Grid::new(9);
+fn load_puzzle(file_path: &str) -> Grid {
+    let mut grid = Grid::new(9);
     let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
     for (y, line) in contents.lines().enumerate() {
         for (x, c) in line.split(' ').enumerate() {
--- src/strategies.rs
+++ src/strategies.rs
@@ -6,8 +6,28 @@ use crate::data::{Grid, Reduction, Solution};
 
 pub trait SolveStrategy {
     fn solutions(grid: &Grid) -> Vec<Solution>;
+
+    fn try_solution(grid: &mut Grid) -> bool {
+        let solutions = Self::solutions(&grid);
+        if !solutions.is_empty() {
+            println!("### Found solutions:");
+            solutions.iter().for_each(|s| println!("{s}"));
+            grid.apply_solutions(&solutions);
+        }
+        !solutions.is_empty()
+    }
 }
 
 pub trait ReduceStrategy {
     fn reduce_candidates(grid: &Grid) -> Vec<Reduction>;
+
+    fn try_reduction(grid: &mut Grid) -> bool {
+        let reductions = Self::reduce_candidates(&grid);
+        if !reductions.is_empty() {
+            println!("### Found reductions:");
+            reductions.iter().for_each(|r| println!("{r}"));
+            grid.apply_reductions(&reductions);
+        }
+        !reductions.is_empty()
+    }
 }
